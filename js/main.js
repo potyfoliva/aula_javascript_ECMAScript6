abad2720e603ac2ce2d83df028de90a67551be57
unknown <potyfoliva@hotmail.com>
2021-01-21 09:48:37 -0300

    aula 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//aula1
 //função com nome
 function log(valor){
 	console.log(valor);
@@ -155,3 +156,107 @@ var obj5 = {
 };
 obj5
 console.log(obj5);
+
+//aula 2
+//rest operator ...   prototype do rest op é um array, disponiblizando os metodos para manipular arrays
+function sum(...args){
+	return args.reduce((acumulador,valor) => acumulador + valor, 0); //O método reduce() executa uma função reducer (fornecida por você) para cada elemento do array, resultando num único valor de retorno.
+}
+console.log(sum(5,4,3,2,1));
+
+//rest op e arrow functions
+const sum2 = (a, b, ...rest) => { //rest op pode pegar parametros restantes passados para a função
+	console.log(a,b,rest);
+	console.log(a + b + rest.reduce((acumulador,valor) => acumulador + valor, 0));
+};
+console.log(sum2(1,2,3,4,5));
+
+//spread operator ... pega tds os itens do array e transformar em parametros para a função
+const multiply = (...args) => args.reduce((acumulado, valor) => acumulado * valor, 1);
+const sum3 = (...spread) => {
+	return multiply(...spread);
+}
+console.log(sum3(2,3,4,5)); //esperado 120
+
+//spread op não se limita a listas
+//pode ser utilizado em strings, arrays, objects(literais) e objetos iteraveis 
+//função de quebrar os itens e repassar
+const str = 'Poty Ferreira';
+function logArgs(...args){ //outra forma seria não passar parametro e usar arguments no console.log
+	console.log(args);
+}
+logArgs(...str);
+
+
+const array = [1,2,3,4];
+function logArgs2(){
+	console.log(arguments);
+}
+logArgs2(...array);
+
+
+//construir arrays
+const array2 = [/*...array],*/ 5,6,7];     //array.concat([5,6,7])
+console.log(array2);
+
+const array3 = [...array2, ...array, 0, 0, 0];
+console.log(array3);
+
+const arrayClone = [...array];
+console.log(arrayClone);
+
+const objSpread = {
+	test:123
+};
+const objSpread2 = {
+	...objSpread,
+	test2: 'teste2'
+};
+console.log(objSpread2);
+//só pode consumir outros objects, pois os objetos literais não são elementos iteraveis
+//não pode usar: const arr = [...objSpread];
+//pode usar para construir outros objetos literais
+const objEx = {
+	test: 123
+};
+const objEx2 = {
+	...objEx,
+	test: 456
+};
+console.log(objEx2);
+
+
+//gerar clones
+const objEx3 = {
+	test: 123
+};
+const objEx4 = {...objEx3};
+console.log(objEx4);
+
+
+//destructuring assignment em reactjs
+//sem destructuring assignment arrays
+var arrayDA = ['maçã', 'banana', 'laranja', ['tomate']];
+var maca1 = arrayDA[0];
+var banana1 = arrayDA[1];
+var laranja1 = arrayDA[2];
+var tomate1 = arrayDA[3][0];
+console.log(maca1, tomate1);
+
+//com destructuring assignment para destruir arrays
+var [maca, banana, laranja, [tomate]] = ['maçã', 'banana', 'laranja', ['tomate']];
+console.log(laranja, tomate);
+
+
+//sem destructuring assignment objects
+var objDA = {
+	name:'Poty'
+}
+var name = objDA.name;
+
+//com destructuring assignment para destruir objects
+var {name} = objDA;
+console.log(name);
+
+
+
